9f257d0043fad1d7403a77e66135f2771ba64c2f
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2020-10-08 12:10:17 -0300

    Commit inicial

---
+++ 01-npm/index.js
@@ -0,0 +1,6 @@
+console.log('Hello node!');
+
+const a = 99;
+const b = 67;
+
+console.log(a + b);
---
+++ 02-nodejs/index.js
@@ -0,0 +1,65 @@
+/**
+ * Objetivos
+ * 0 - Obter um usuário
+ * 1 - Obter o número de telefone de um usuário pelo seu Id
+ * 2 - Obter o endereço do usuário pelo seu Id
+ */
+
+function obterUsuario(callback) {
+    setTimeout(function () {
+        return callback(null, {
+            id: 1,
+            nome: 'Aladin',
+            dataNascimento: new Date()
+        });
+    }, 1000);
+}
+
+function obterTelefone(idUsuario, callback) {
+    setTimeout(() => {
+        return callback(null, {
+            telefone: '988752339',
+            ddd: 91
+        });
+    }, 2000);
+}
+
+function obterEndereco(idUsuario, callback) {
+    setTimeout(() => {
+        return callback(null, {
+            rua: 'conselheiro furtado',
+            numero: 3409
+        });
+    }, 2000);
+}
+
+function resolverUsuario(erro, usuario) {
+    console.log('usuario ', usuario);
+}
+
+obterUsuario(function resolverUsuario(error, usuario) {
+    if (error) {
+        console.error("Deu Ruim em Usuario ", error);
+        return;
+    }
+
+    obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+        if (error1) {
+            console.error("Deu Ruim em Telefone ", error1);
+            return;
+        }
+
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco) {
+            if (error2) {
+                console.error("Deu Ruim em Endereço ", error2);
+                return;
+            }
+
+            console.log(`
+                Nome: ${usuario.nome},
+                Endereco: ${endereco.rua},${endereco.numero},
+                Telefone: (${telefone.ddd})${telefone.telefone}
+            `);
+        });
+    });
+});
