9131ec31b717bbf6fe75c75a1ac0523e997fb40c
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2020-10-11 19:23:13 -0300

    complicando o que já era simples

--- 04-lists/for-forin/2.map_socker.js
+++ 04-lists/for-forin/2.map_socker.js
@@ -1,15 +1,35 @@
 const service = require('./service_socker');
 
+Array.prototype.meuMap = function(callback) {
+    const novoArrayMapeado = [];
+    for(let i = 0; i <= this.length; i++) {
+        const resultado = callback(this[i], i);
+        novoArrayMapeado.push(resultado);
+    }
+
+    return novoArrayMapeado;
+}
+
 async function main() {
     try {
         const results = await service.obterTimes('a');
-        const nameTeams = [];
+        //const nameTeams = [];
         
-        results.teams.forEach(function (item) {
-            nameTeams.push(item.strAlternate);
+        // results.teams.forEach(function (item) {
+        //     nameTeams.push(item.strAlternate);
+        // });
+
+        // const nameTeams = results.teams.map(function(times) {
+        //     return times.strAlternate;
+        // });
+
+        //const nameTeams = results.teams.map((times) => times.strAlternate);
+
+        const nameTeams = results.teams.meuMap(function(times, indice) {
+            return times.teams.strAlternate;
         });
 
-        console.log(nameTeams);
+        console.log('Nome do Time:', nameTeams);
     } catch (error) {
         console.error('Deu Ruim: ', error);
     }
