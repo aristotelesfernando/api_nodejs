41868740898e6c48c31b623cae311aa9d4cc4d79
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2020-10-12 11:07:27 -0300

    implemnatndo o filter manualmente

--- 04-lists/for-forin/3.filter.js
+++ 04-lists/for-forin/3.filter.js
@@ -1,28 +1,45 @@
 const { obterPessoas } = require('./service');
 
+Array.prototype.meuFilter = function(callback) {
+    const lista = [];
+    for(index in this) {
+        const item = this[index];
+        const result = callback(item, index, this);
+        //relembrando que 0, "", null, undefined === false
+        if(!result) continue;
+        lista.push(item);
+    }
+    return lista;
+};
+
 async function main() {
     try {
         const {
             results
         } = await obterPessoas('a');
 
-        const familiaLars = results.filter(function(item) {
-            /**
-                por padrão precisa retornar um booleano
-                para informar se deve manter ou remover da lista
-                false > remove da lista
-                true > mantem
+        // const familiaLars = results.filter(function(item) {
+        //     /**
+        //         por padrão precisa retornar um booleano
+        //         para informar se deve manter ou remover da lista
+        //         false > remove da lista
+        //         true > mantem
 
-                não encontrou é igual a -1
-                encontrou retorna a posição no array
-            */
-            const result = item.name.toLowerCase().indexOf('lars') !== -1;
-            return result;
+        //         não encontrou é igual a -1
+        //         encontrou retorna a posição no array
+        //     */
+        //     const result = item.name.toLowerCase().indexOf('lars') !== -1;
+        //     return result;
+        // });
+
+        const familiaLars = results.meuFilter((item, index, lista) => {
+            console.log(`index: ${index}`, lista.length);
+            return item.name.toLowerCase().indexOf('lars') !== -1;
         });
 
         const names = familiaLars.map((pessoa) => pessoa.name);
         console.log(names);
-        
+
     } catch (error) {
         console.error('Ocorreu um erro: ', error);
     }
