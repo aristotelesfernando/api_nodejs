ae101263862b9990578ee7424ec7a1c89e4cdb69
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2020-10-12 21:02:52 -0300

    implemnetando reduce personalizado

--- 04-lists/for-forin/4.reduce.js
+++ 04-lists/for-forin/4.reduce.js
@@ -1,5 +1,14 @@
 const { obterPessoas } = require('./service');
 
+Array.prototype.meuReduce = function(callback, valorInicial)
+{
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0];
+    for(let i = 0; i <= this.length; i++) {
+        valorFinal = callback(valorFinal, this[i], this);
+    }
+    return valorFinal;
+}
+
 async function main() {
     try {
         const { results } = await obterPessoas('a'); 
@@ -7,9 +16,19 @@ async function main() {
         const pesos = results.map(item => parseInt(item.height));
         console.log('pesos ', pesos);
 
-        const total = pesos.reduce((anterior, proximo) => {
-            return anterior + proximo;
-        }, 0);
+        // const total = pesos.reduce((anterior, proximo) => {
+        //     return anterior + proximo;
+        // }, 0);
+
+        const minhaLista = [
+            ['Fernando','Oliveira'],
+            ['NodeBR','Nerdzão']
+        ];
+
+        const total = minhaLista.meuReduce((anterior,proximo) => {
+            return anterior.concat(proximo);
+        },[])
+        .join(', ');
 
         console.log('total:', total);
     } catch (error) {
