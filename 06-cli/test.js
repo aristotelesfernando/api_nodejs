9bafe5ebd69e4f92a269c10f7e8904713bb53835
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2020-10-19 19:58:19 -0300

    Atulizar não funcionou... iniciando outra aula

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -7,10 +7,13 @@ const database = require('./database');
 
 const DEFAULT_ITEM_CADASTRAR = {nome:'Flash', poder:'speed', id: 1};
 
+const DEFAULT_ITEM_ATUALIZAR = {nome:'Lanterna Verde', poder:'energia do anel', id: 2};
+
 describe('Suite de manipulação de herois', () => {
 
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
     });
 
     it('deve pesquisar um heroi pesquisando arquivos', async () => {
@@ -29,6 +32,29 @@ describe('Suite de manipulação de herois', () => {
         const [atual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
 
         deepEqual(atual, expected);
+    });
+
+    it('deve remover um heroi por ID', async() => {
+        const expected = true;
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+        deepEqual(resultado, expected);
+    });
+
+    it('deve atualizar o heroi por ID', async () => {
+        const expected = {
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        };
+
+        const novoDado = {
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        };
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado);
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id);
+
+        deepEqual(resultado, expected);
     });
 });
 
